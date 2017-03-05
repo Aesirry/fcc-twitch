d48bc492489b420724996f5cecc9a6058f3ed053
Joe Beltrami <jpbeltrami@gmail.com>
2017-03-05 16:11:41 +0800

    change some css, start basic JS script structure

--- scripts.js
+++ scripts.js
@@ -1,6 +1,30 @@
-// make bgcolor of online profile green
-// others are gray
 // sort continuously with online profiles on top
 // have to have freecodecamp profiles, brunofin, comster404
 // click on status output and go to the stream directly
 // can see additional details about what the streamer is streaming.
+
+// JSON request to get freecodecamp channel:
+//
+// check streams https://wind-bow.gomix.me/twitch-api/streams/freecodecamp?callback=?
+// check users https://wind-bow.gomix.me/twitch-api/users/freecodecamp?callback=?
+// check channel https://wind-bow.gomix.me/twitch-api/channels/freecodecamp?callback=?
+
+
+$(document).ready(function() {
+  var profiles = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+    for (var i = 0; i < profile.length; i++) {
+      // get stream info
+      $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + profiles[i] + '?callback=?', function(data) {
+        console.log(data);
+      });
+      // get user info
+      $.getJSON('https://wind-bow.gomix.me/twitch-api/users/' + profiles[i] + '?callback=?', function(data) {
+        console.log(data);
+      });
+      // get channel
+      $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/' + profiles[i] + '?callback=?', function(data) {
+        console.log(data);
+      });
+    }
+  });
+});
