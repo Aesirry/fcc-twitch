1cc8dc5c4f5dc132de4e502318177721a361e530
Joe Beltrami <jpbeltrami@gmail.com>
2017-03-13 16:45:19 +0800

    fixed panels, add condition for status

--- scripts.js
+++ scripts.js
@@ -13,26 +13,34 @@
 // from streams get stream (null is offline), status.
 // to see the stream just use plain old url like https://www.twitch.tv/summit1g
 
-
-// $(document).ready(function() {
-//   var profiles = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "dota2ruhub"];
-//     for (var i = 0; i < profiles.length; i++) {
-//       $.getJSON('https://wind-bow.gomix.me/twitch-api/users/' + profiles[i] + '?callback=?', function(data) {
-//           var profile_home = 'https://www.twitch.tv/' + data.name;
-//           var results = '<div class="panel panel-default">';
-//           results = '<div class="panel-body">';
-//           results += '<img src="'+ data.logo + '" alt="' + data.display_name + ' \'s logo" class="img-circle user-logo" />';
-//           results += '<a href="' + profile_home + '" target="_blank" >See Profile</a>';
-//           results += '</div></div>';
-//           $(".users").append(results);
-//           $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + profiles[i] + '?callback=?', function(data) {
-//             if (data.stream != null){
-//               $('.panel-body').addClass('online');
-//             }
-//             else {
-//               $('.panel-body').removeClass('online').addClass('offline');
-//             }
-//           });
-//         });
-//       }
-// }); //document.ready
+$(document).ready(function() {
+  var profiles = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "dota2ruhub"];
+    for (var i = 0; i < profiles.length; i++) {
+      $.getJSON('https://wind-bow.gomix.me/twitch-api/users/' + profiles[i] + '?callback=?', function(data) {
+          var profile_home = 'https://www.twitch.tv/' + data.name;
+          var result = '<div class="panel user online">';
+          result += '<div class="panel-body row">';
+          result += '<div class="col-md-1">';
+          result += '<img class="img-circle user-logo" src="' + data.logo + '" />';
+          result += '</div>';
+          result += '<div>';
+          result += '<div class="col-md-9 text-container">';
+          result += '<h4>' + data.display_name + '</h4>';
+          // result += '<span>' + data.channel.status + '</span><br />'; this comes from the stream request
+          result += '<a href="' + profile_home + '" target="_blank" class="btn btn-info str-btn" role="button">Go to Stream</a>';
+          result += '</div>';
+          result += '</div>';
+          result += '</div>';
+          result += '</div>';
+          $(".users").append(result);
+          // $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + profiles[i] + '?callback=?', function(data) {
+          //   if (data.stream != null){
+          //     $('.panel-body').addClass('online');
+          //   }
+          //   else {
+          //     $('.panel-body').removeClass('online').addClass('offline');
+          //   }
+          // });
+        });
+      }
+}); //document.ready
